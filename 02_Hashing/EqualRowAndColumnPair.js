cd7acea9758ec5dfe1ba687c532309c39d549912
FabioKallina <fabiokdp8@gmail.com>
2025-06-24 19:34:15 -0700

    Hash

--- 02_Hashing/EqualRowAndColumnPair.js
+++ 02_Hashing/EqualRowAndColumnPair.js
@@ -10,38 +10,36 @@ Output: 1 => [2, 7, 7] appears once as column and once as row
  */
 /**
  * @param {number[][]} grid
- * @returns {number}
+ * @return {number}
  */
 var equalPairs = function(grid) {
-    //define a method that can be used to covert an array to a key
     let convertToKey = arr => {
         let key = "";
-        for ( const num of arr ) {
-            key = num + ",";
+        for (const n of arr) {
+            key += n + ","
         }
+        return key;
     }
     let map1 = new Map();
     for ( const arr of grid ) {
         let key = convertToKey(arr);
-        if (!map1.has(key)) {
-            map1.set(key, (map1.get(key) || 0) + 1);
-        }
+        map1.set(key, (map1.get(key) || 0) + 1);
     }
     let map2 = new Map();
-    for ( col = 0; col < grid[0].length; col++ ) {
+    for ( let col = 0; col < grid[0].length; col++ ) {
         let currCol = [];
         for ( let row = 0; row < grid.length; row++ ) {
             currCol.push(grid[row][col]);
         }
         let key = convertToKey(currCol);
-        map2.set(key, (map2.get(key) || 0) + 1)
+        map2.set(key, (map2.get(key) || 0) + 1); 
     }
     let ans = 0;
     for ( const [key, val] of map1 ) {
         ans += val * map2.get(key) || 0;
     }
     return ans;
-}
+};
 /** Time and Space Complexity
  * Time: O(n^2)
  * Space: O(n^2)
