dd502bc86ca18da6d53374a1bf965910050d7a99
FabioKallina <fabiokdp8@gmail.com>
2025-07-14 14:49:52 -0700

    Dynamic Programming

---
+++ 10_DynamicProgramming/ClimbingStairs.js
@@ -0,0 +1,38 @@
+
+/**
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+*/
+/** Iteratively
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if ( n <= 2 ) return n;
+    let dp = new Array(n + 1).fill(0); //n + 1 bc if n = 2, then dp = [0, 0] instead of [0, 0, 0]
+    dp[1] = 1; //[0, 1, 0]
+    dp[2] = 2; //[0, 1, 2]
+
+    for ( let i = 3; i <= n; i++ ) {
+        dp[i] = dp[i - 1] + dp[i - 2]; //ex dp[3] = dp[3 - 1] + dp[3 - 2] => dp[2] + dp[1] => 2 + 1 => dp[3] = 3
+                                      //ex dp[4] = dp[4 - 1] + dp[4 - 2] => dp[3] + dp[2] => 2 + 1 => dp[4] = 5
+    }
+    return dp[n];
+};
---
+++ 10_DynamicProgramming/HouseRobber.js
@@ -0,0 +1,67 @@
+
+/**
+You are a professional robber planning to rob houses along a street. 
+Each house has a certain amount of money stashed, 
+the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected 
+and it will automatically contact the police if two adjacent houses were broken into on the same night.
+Given an integer array nums representing the amount of money of each house, 
+return the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+
+Example 2:
+
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+*/
+/** Recursively
+ * @param {number[]} nums
+ * @returns {number}
+ */
+var rob = function(nums) {
+    let dp = i => {
+        //Base cases
+        //If there's only one house
+        if ( i == 0 ) return nums[0];
+        //If there's only 2 houses, rob the one with most money
+        if ( i == 1 ) {
+        return Math.max(nums[0], nums[1]);
+        }
+        if ( memo[i] != -1 ) {
+            return memo[i];
+        }
+        //Recurrence relation
+        memo[i] = Math.max(dp(i - 1), dp(i - 2) + nums[i]);
+        return memo[i];
+    }
+    let memo = new Array(nums.length).fill(-1);
+    return dp(nums.length - 1);
+}
+
+/** Iteratively
+ * @param {number[]} nums
+ * @returns {number}
+ */
+var rob = function(nums) {
+    if ( nums.length == 1 ) {
+        //to prevent out of bounds error
+        return nums[0];
+    }
+    let n = nums.length;
+    let dp = new Array(n).fill(0);
+    //Base cases
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+
+    for ( let i = 2; i < n; i++ ) {
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+    }
+    return dp[n - 1];
+}
